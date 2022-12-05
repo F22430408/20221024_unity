60901422446e98104a76a64f0fe6f76fc416bf96
F22430408 <116561790+F22430408@users.noreply.github.com>
2022-12-05 21:34:11 +0800

    Updatet程式介紹

--- Assets/Script/FirstScript.cs
+++ Assets/Script/FirstScript.cs
@@ -1,18 +1,20 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;//檔案名稱和類別名稱相同
 
 public class FirstScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	//整理程序 ctrl+K+D
+	//縮排按鍵 tab
+	private void Awake()
+	{
+		print("哈瞜你好嗎");
+		print("衷心感謝");
+		print("珍重再見");
+		print("期待再相逢");
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	private void Start()
+	{
+		print("<color=yellow>我是誰</color>");
+		print("<color=yellow>你是誰</color>");
+	}
 }
---
+++ Assets/Script/LearnField.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+/// <summary>
+/// 摘要
+/// </summary>
+public class LearnField : MonoBehaviour
+{
+ 
+}
